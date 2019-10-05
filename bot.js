65793e884564aae2c4c66a733219a27036054351
Glitch (phenakist-fmcord) <none>
2019-10-05 19:36:13 +0000

    🍮👝 Checkpoint ./.gitignore:7399714/46 ./bot.js:7399714/559

--- bot.js
+++ bot.js
@@ -8,6 +8,23 @@ const canvas = require(`canvas`);
 
 const app = express();
 
+// init Spotify API wrapper
+
+const SpotifyWebApi = require(`spotify-web-api-node`);
+
+const spotifyApi = new SpotifyWebApi({
+  clientId : process.env.SPOTIFY_CLIENT_ID,
+  clientSecret : process.env.SPOTIFY_CLIENT_SECRET,
+});
+
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err.message);
+  });
+
 if (process.platform === `win32`) {
   process.env.PWD = process.cwd();
   // NOTE: Inconsolata font is not as good at covering non-ASCII symbols as Noto Sans is. 
