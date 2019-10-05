c45f2f67d06a283044058b7ef0152059c007513c
Glitch (phenakist-fmcord) <none>
2019-10-05 19:01:08 +0000

    💍🍭 Checkpoint ./src/commands/imlucky.js:7399714/2664

---
+++ src/classes/List.js
@@ -0,0 +1,16 @@
+ array with a getter of a random value.
+ * @public 
+ */
+
+class List extends Array {
+  
+  constructor(...args) {
+    super(...args);
+  }
+
+  get random() {
+    return this[Math.floor(Math.random() * this.length)];
+  }
+}
+
+module.exports = List;
